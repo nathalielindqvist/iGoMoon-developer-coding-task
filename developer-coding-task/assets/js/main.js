2f4131d222229c64bf4798d0bae40c909d7fbb86
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-12-06 17:23:17 +0100

    Functionality for hiding/showing fields

--- developer-coding-task/assets/js/main.js
+++ developer-coding-task/assets/js/main.js
@@ -1,3 +1,20 @@
-(function ($) { 
-	
-})(jQuery);
+$('#firstName').hide();
+$('#lastName').hide();
+$('#submitButton').hide();
+
+document.addEventListener('input',function() {
+document.querySelector("#inputEmail").onchange=check;},false);
+
+
+function check(event) {
+ if ($("#inputEmail").val()) {
+      $('#firstName').show();
+      $('#lastName').show();
+      $('#submitButton').show();
+
+    } else {
+      $('#firstName').hide();
+      $('#lastName').hide();
+      $('#submitButton').hide();
+    }
+   }
