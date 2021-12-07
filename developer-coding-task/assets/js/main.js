0b5cdaeab49fc5adfdf849d219f869e1022c4ae4
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-12-07 09:16:50 +0100

    Add fade-in to fields when shown

--- developer-coding-task/assets/js/main.js
+++ developer-coding-task/assets/js/main.js
@@ -5,12 +5,11 @@ $('#submitButton').hide();
 document.addEventListener('input',function() {
 document.querySelector("#inputEmail").onchange=check;},false);
 
-
 function check(event) {
  if ($("#inputEmail").val()) {
-      $('#firstName').show();
-      $('#lastName').show();
-      $('#submitButton').show();
+      $('#firstName').fadeIn(200);
+      $('#lastName').fadeIn(200);
+      $('#submitButton').fadeIn(200);
 
     } else {
       $('#firstName').hide();
